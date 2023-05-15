f7d9c6a479952b9187ef3e3cbdab034895d7108c
Sayaki-M <38837489+Sayaki-M@users.noreply.github.com>
2023-05-15 16:56:24 +0900

    add notionに追加する機構を作成

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const port = 3000;
 const axios = require("axios");
 require("dotenv").config();
+const { Client } = require("@notionhq/client");
+
+notion = new Client({
+  auth: process.env.NOTION_API_KEY,
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -11,18 +16,133 @@ app.get("/", (req, res) => {
 app.get("/paper", (req, res) => {
   axios
     .get(
-      `https://api.semanticscholar.org/graph/v1/paper/${process.env.PAPER_ID}/citations`
+      `https://api.semanticscholar.org/graph/v1/paper/${process.env.PAPER_ID}/citations`,
+      {
+        params: {
+          limit: 500,
+          //fields:"isInfluential"
+        },
+      }
     )
     .then((response) => {
       axios
-        .post(`https://api.semanticscholar.org/graph/v1/paper/batch`, {
-          params: { fields: `paperId,title` },
-          ids: response.data.data.map((i) => i.citingPaper.paperId),
-        })
+        .post(
+          "https://api.semanticscholar.org/graph/v1/paper/batch?fields=url,title,authors,citationCount,fieldsOfStudy,tldr",
+          {
+            ids: response.data.data.map((i) => i.citingPaper.paperId),
+          }
+        )
         .then((response) => {
-          console.log(response.data);
+          response.data.forEach((paper) => {
+            (async () => {
+              const res = await notion.pages.create({
+                parent: {
+                  type: "database_id",
+                  database_id: "af2b39d30bb049488affe02f692ad57b",
+                },
+                properties: {
+                  title: {
+                    title: [
+                      {
+                        text: {
+                          content: paper.title,
+                        },
+                      },
+                    ],
+                  },
+                  authors: {
+                    multi_select: paper.authors.map((author) => ({
+                      name: author.name,
+                    })),
+                  },
+                  URL: {
+                    url: paper.url,
+                  },
+                  citationCount: {
+                    number: paper.citationCount,
+                  },
+                  tldr: paper.tldr
+                    ? paper.tldr.text
+                      ? {
+                          rich_text: [
+                            {
+                              text: {
+                                content: paper.tldr.text,
+                              },
+                            },
+                          ],
+                        }
+                      : undefined
+                    : undefined,
+                  fieldsOfStudy: paper.fieldsOfStudy
+                    ? {
+                        multi_select: paper.fieldsOfStudy.map((field) => ({
+                          name: field,
+                        })),
+                      }
+                    : undefined,
+                },
+              });
+            })();
+
+            (async () => {
+              await new Promise((resolve) => setTimeout(resolve, 1000));
+            })();
+          });
         });
     });
+});
+
+app.get("/sampleadd", (req, res) => {
+  (async () => {
+    const res = await notion.pages.create({
+      parent: {
+        type: "database_id",
+        database_id: "af2b39d30bb049488affe02f692ad57b",
+      },
+      properties: {
+        title: {
+          title: [
+            {
+              text: {
+                content: "ねこのせいたい",
+              },
+            },
+          ],
+        },
+        authors: {
+          multi_select: [{ name: "nekodanekoo", name: "inuyamainuko" }].map(
+            (author) => ({
+              name: author.name,
+            })
+          ),
+        },
+        URL: {
+          url: "https://www.google.com",
+        },
+        citationCount: {
+          number: 23,
+        },
+        tldr: undefined,
+        //  {
+        //   rich_text: [
+        //     {
+        //       text: {
+        //         content: null,
+        //         link: null,
+        //       },
+        //       //plain_text: "ねこのせいたいについてかいたものです",
+        //     },
+        //   ],
+        // },
+        fieldsOfStudy: {
+          multi_select: ["せいぶつがく", "りゅうたいりきがく"].map((field) => ({
+            name: field,
+          })),
+        },
+      },
+    });
+  })();
 });
 
 app.listen(port, () => {
